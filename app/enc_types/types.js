70207e9ef3d21db9a1b39aa6a1daca0641c49894
Lagutov Alexander <lagutov.99@mail.ru>
2021-06-12 21:26:56 +0300

    decrypt + refactor

--- app/enc_types/types.js
+++ app/enc_types/types.js
@@ -1,8 +1,8 @@
 types = {
     stegoImg: { key: '1', text: 'Стеганография в PNG' },
-    cryptoAES: { key: '2', text: 'Шифрование AES'},
-    cryptoRSA: { key: '3', text: 'Шифрование 3DES'},
-    stegoWAV: { key: '4', text: 'Стеганография в WAV'},
+    cryptoAES: { key: '2', text: 'Шифрование AES' },
+    cryptoDES: { key: '3', text: 'Шифрование 3DES' },
+    stegoWAV: { key: '4', text: 'Стеганография в WAV' },
     stegoMP3: { key: '5', text: 'Стеганография в MP3' }
 }
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const { redirect, sendfile } = require('express/lib/response')
-const router = express.Router()
+const express = require('express'),
+    { redirect, sendfile } = require('express/lib/response'),
+    router = express.Router()
+
 const http = require('http'),
     formidable = require('formidable'),
     util = require('util'),
     fs = require('fs')
 
 const tempFolder = './temp/'
+
 const encDir = '../enc_types/'
-const types = require(encDir + 'types') 
+const types = require(encDir + 'types')
 
 const stegoImg = require(encDir + 'helper/img'),
     AES = require(encDir + 'helper/AES'),
@@ -23,51 +25,95 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
     const form = formidable.IncomingForm({ multiples: true })
 
-    form.parse(req, (err, fields, files) => {
-        const msg = fields.msg,
-            key = fields.key,
-            encType = fields.enc_type
-        console.log(fields)
+    let msg, key, encType, asset
 
-        if (!msg) {
-            console.log(msg)
-            return res.redirect('/#shifr-page') 
-        }
+    form.parse(req, (err, fields, files) => {
+        msg = fields.msg
+        key = fields.key
+        encType = fields.enc_type
+        asset = files.asset
+        crypt = true ? fields.encrypt : false
+        console.log('CRY', crypt)
+
+        console.log(fields, asset)
         switch (encType) {
             case types.stegoImg.key: {
-                img = stegoImg.encrypt(files.asset.path, msg)
-                console.log(img) 
-                enc_img = sendFile(res, img, 'stegoImg', 'png')
-                console.log(stegoImg.decrypt(enc_img))
+                stegoImgHandle(res, crypt, msg, asset)
                 break
             }
+
             case types.cryptoAES.key: {
-                cipher = AES.enAES(msg, key)
-                cipher_file = sendFile(res,cipher,'cryptoAES','txt')
-                Mydecipher = AES.decAES(cipher,key)
-                console.log(Mydecipher)
+                try {
+                    cryptoAESHandle(res, crypt, msg, key)
+                } catch (error) {
+                    console.log(error)
+                    res.redirect('/#shifr-page')
+                }
                 break
             }
+
             case types.stegoWAV.key: {
-                wav = WAV.enWAV(msg, files.asset.path)
-                console.log(wav)
-                enc_wav = sendFile(res, wav, 'stegoWAV', 'wav')
-                /* dec_wav = WAV.decWAV(key, files.asset.path)
-                console.log(dec_wav) */
+                stegoWAVHandle(res, crypt, msg, asset)
                 break
             }
-            case types.cryptoDES.key:{
-                
+
+            case types.cryptoDES.key: {
+                cryptoDESHandle(res, crypt, msg, key)
+                break
             }
+
             default: {
-                console.log(encType)
-                return res.redirect('/#shifr-page')
+                console.log('Enc Type', encType)
+
+                res.redirect('/#shifr-page')
             }
         }
     })
 })
 
+function stegoImgHandle(res, sw, msg, asset) {
+    console.log('StegoImg')
+    if (!asset) return
+    if (sw) {
+        resImg = stegoImg.encrypt(asset.path, msg)
+    } else {
+        resImg = stegoImg.decrypt(asset.path)
+    }
+    file = sendFile(res, resImg, 'stegoImg', 'png')
+    console.log(resImg, file)
+}
+function stegoWAVHandle(res, sw, msg, asset) {
+    console.log('WAV')
+    if (!asset) return
+    if (sw) {
+        resWav = WAV.enWAV(msg, asset.path)
+    } else {
+        resWav = WAV.decWAV(asset.path)
+    }
+    file = sendFile(res, resWav, 'stegoWAV', 'wav')
+    console.log(resWav, file)
+}
+
+function cryptoAESHandle(res, sw, msg, key) {
+    console.log('AES')
+    if (sw) {
+        console.log('Enc')
+        resMsg = AES.enAES(msg, key)
+    } else {
+        console.log('Dec')
+        resMsg = AES.decAES(msg, key)
+    }
+
+    file = sendFile(res, resMsg, 'cryptoAES', 'txt')
+    console.log(resMsg, file)
+}
+
+function cryptoDESHandle(res, sw, msg, key) {
+    console.log('DES')
+}
+
 function sendFile(res, file, name, ext) {
+    console.log('Sending File')
     if (!fs.existsSync(tempFolder)) {
         fs.mkdirSync(tempFolder)
     }
